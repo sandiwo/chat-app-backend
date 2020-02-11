fb3131356f6b182f100a2b4237e3a9ffe11d7e3a
Sandi Sahdewo <sandi.sahdewo@gmail.com>
2020-02-11 22:25:49 +0700

    refactor upload image avatar

--- controllers/AvatarsController.js
+++ controllers/AvatarsController.js
@@ -1,6 +1,19 @@
 var Avatar = require('../models/Avatar')
 
 module.exports = {
+  upload: async (req, res) => {
+    try{
+      await Avatar.query().insert({
+        name: req.file.originalname,
+        user_id: req.auth().id,
+        directory: `${req.file.destination}/${req.file.originalname}`,
+        uploaded_at: new Date
+      })
+      res.send(req.file)
+    } catch(err) {
+      res.send(err.message)
+    }
+  },
   lastUploaded: async (req, res) => {
     await Avatar.query().where({user_id: req.auth().id, deleted_at: null}).orderBy('uploaded_at', 'DESC')
       .then(response => {
--- routes/AvatarRoute.js
+++ routes/AvatarRoute.js
@@ -2,25 +2,13 @@ var express = require('express');
 var router = express.Router();
 const passport = require('passport');
 var multer = require('../libraries/multer')
-var Avatar = require('../models/Avatar')
 var AvatarsController = require('../controllers/AvatarsController')
 
 router.post('/upload', 
   [
     passport.authenticate('jwt', {session: false}),
-    multer.upload.single('avatar'), async (req, res, next) => {
-      try{
-        await Avatar.query().insert({
-          name: req.file.originalname,
-          user_id: req.auth().id,
-          directory: `${req.file.destination}/${req.file.originalname}`,
-          uploaded_at: new Date
-        })
-        res.send(req.file)
-      } catch(err) {
-        res.send(err.message)
-      }
-    }
+    multer.upload.single('avatar'), 
+    AvatarsController.upload
   ]
 );
 
